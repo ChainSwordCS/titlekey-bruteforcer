70d3bb019441750eaf408c014c0e72626501ca6f
ChainSwordCS <chainswordcs@gmail.com>
2025-05-15 20:51:44 -0400

    wip

--- bruteforce.py
+++ bruteforce.py
@@ -4,9 +4,19 @@
 
 import itertools
 import string
+import json
+import argparse
+
 import keygen
 import wiiu_decrypt
 import binascii
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--system', help='valid options: \'wiiu\', \'dsi\'\n'+'not yet implemented: \'wii\', \'3ds\'')
+parser.add_argument('--commonkey', help='choose a commonkey from ckey.json, in case the automatic choice is wrong. valid options: \'dsi_prod\', \'dsi_dev\', \'dsi_debugger\', \'wiiu_prod\', \'wiiu_dev\'')
+parser.add_argument('--commonkeyoverride', help='manually specify the commonkey')
+parser.add_argument('titleid')
+args = parser.parse_args()
 
 def bruteforce(tid, ckey):
     chars = "0123456789"
@@ -32,6 +42,24 @@ def bruteforce(tid, ckey):
     return
 
 
-tid = '00050000101b9800'
+tid = args.titleid
+
+if args.system:
+    match args.system:
+        case 'wiiu':
+            print('wiiu')
+        case 'dsi':
+            print('dsi')
+        case _:
+            sys.exit('Error: invalid argument passed in --system')
+else:
+    sys.exit('Error: system autodetection based on titleid not yet implemented! please use the --system argument.')
+
+if args.commonkeyoverride:
+    ckey = args.commonkeyoverride
+else:
+    
+
 ckey = keygen.get_ckey()
-bruteforce(tid, ckey)
+#bruteforce(tid, ckey)
+bruteforce_dsi(tid, ckey)
