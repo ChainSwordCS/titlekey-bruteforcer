2a6506579d0e57ecf56510e7df79552502116df8
Ethan Chapman <ethan@we3chapmans.com>
2025-05-16 00:52:11 -0400

    Broken multiprocessing code
    It doesn't work, but it doesn't crash

--- bruteforce.py
+++ bruteforce.py
@@ -3,15 +3,18 @@
 # License: MIT
 
 import itertools
-import string
 import json
 import argparse
 import binascii
 import sys
+import multiprocessing
+import string
+import time
 
 import keygen
 import wiiu_decrypt
 
+# Argument parser setup
 parser = argparse.ArgumentParser()
 parser.add_argument('--system', help='valid options: \'wiiu\', \'dsi\'\n'+'not yet implemented: \'wii\', \'3ds\'')
 parser.add_argument('--commonkey', help='choose a commonkey from ckey.json, in case the automatic choice is wrong. valid options: \'dsi_prod\', \'dsi_dev\', \'dsi_debugger\', \'wiiu_prod\', \'wiiu_dev\'')
@@ -19,25 +22,52 @@ parser.add_argument('--commonkeyoverride', help='manually specify the commonkey'
 parser.add_argument('titleid')
 args = parser.parse_args()
 
+# Threading setup
+QUEUE_MAX_SIZE = 1000 # Strict
+QUEUE_MIN_SIZE = 500  # Target
+NUM_WORKERS = multiprocessing.cpu_count() # Probably about optimal, note this coes not directly correspond to core usage
+
+COMMON_PASSES = ["mypass", "5678", "1234567890", "nintendo", "test"]
+
+manager = None
+data_queue = None
+decoded_event = None
+passes_done_event = None
+
 def bruteforce_wiiu(tid, ckey):
     chars = "0123456789"
-    attempts = 0
     contents, title_id = wiiu_decrypt.get_contents(tid)
     app_data = wiiu_decrypt.get_app_data(tid, contents)
-    for length in range(5, 10):
-        for guess in itertools.product(chars, repeat=length):
-            attempts += 1
-            guess = ''.join(guess)
-            if attempts % 100 == 0:
-                print(guess)
-            unencrypted_keyguess, encrypted_keyguess = keygen.encrypt_guess(tid, guess, ckey)
-            result = wiiu_decrypt.decrypt(tid, encrypted_keyguess, ckey, contents, title_id, app_data)
-            if (result == 1):
-                print('bruteforce success after '+str(attempts)+' attempts')
-                print('password: '+guess)
-                print('encrypted titlekey: '+encrypted_keyguess)
-                print('decrypted titlekey: '+binascii.hexlify(unencrypted_keyguess).decode())
-                return
+    # attempts = 0
+    # for length in range(5, 10):
+    #     for guess in itertools.product(chars, repeat=length):
+    #         attempts += 1
+    #         guess = ''.join(guess)
+    #         if attempts % 100 == 0:
+    #             print(guess)
+    #         unencrypted_keyguess, encrypted_keyguess = keygen.encrypt_guess(tid, guess, ckey)
+    #         result = wiiu_decrypt.decrypt(tid, encrypted_keyguess, ckey, contents, title_id, app_data)
+    #         if (result == 1):
+    #             print('bruteforce success after '+str(attempts)+' attempts')
+    #             print('password: '+guess)
+    #             print('encrypted titlekey: '+encrypted_keyguess)
+    #             print('decrypted titlekey: '+binascii.hexlify(unencrypted_keyguess).decode())
+    #             return
+    
+    # Start worker processes
+    workers = []
+    for i in range(NUM_WORKERS):
+        p = multiprocessing.Process(target=wiiu_process_guesses, args=(i, contents, title_id, app_data, tid, ckey))
+        p.start()
+        workers.append(p)
+    
+    try:
+        get_guesses(data_queue, chars, 1, 5, 0, True)
+    except KeyboardInterrupt:
+        decoded_event.set() # Not actually decoded but it shuts down all the stuff
+    
+    for p in workers:
+        p.join()
     
     print('bruteforce failed...')
     return
@@ -46,65 +76,134 @@ def bruteforce_dsi(tid, ckey):
     chars = "0123456789"
     attempts = 0
 
+def get_guesses(chars = string.printable.strip(), minsize = 1, maxsize = 5, offset = 0, use_common = True):
+    attempts = 0
+    size = minsize
+    if use_common and offset >= len(COMMON_PASSES):
+        offset -= len(COMMON_PASSES)
+    elif use_common:
+        size = 0
+    for i in range(minsize, maxsize):
+        if len(chars)**i <= offset:
+            offset -= len(chars) ** i
+            size += 1
+        else:
+            break
+    
+    while not decoded_event.is_set() and not passes_done_event.is_set():
+        if data_queue.qsize() < QUEUE_MIN_SIZE:
+            for _ in range(QUEUE_MAX_SIZE - data_queue.qsize()):
+                #item = random.randint(0, 100)
+                if size == 0:
+                    item = COMMON_PASSES[offset]
+                else:
+                    item = ""
+                    v = offset
+                    for i in range(size):
+                        item = chars[v%len(chars)] + item
+                        v //= len(chars)
+                try:
+                    data_queue.put((item, attempts), timeout=1)
+                    print(f"[Producer] Enqueued {item}")
+                    attempts += 1
+                except data_queue.Full:
+                    break
+                offset += 1
+                attempts += 1
+                if (size == 0 and offset == len(COMMON_PASSES)) or offset == len(chars)**size:
+                    offset = 0
+                    size += 1
+                    if size < maxsize:
+                        passes_done_event.set()
+                        break
+        time.sleep(0.25) # Give them some time
 
+def wiiu_process_guesses(worker_id, contents, title_id, app_data, tid, ckey):
+    """Worker processes items until the queue is empty and producer is done."""
+    while True:
+        if decoded_event.is_set():
+            break
+        try:
+            guess, attempt = data_queue.get(timeout=1) # Should be longer than the guess generator's sleep
+            print(f"[Worker {worker_id}] Processing {guess}")
+            unencrypted_keyguess, encrypted_keyguess = keygen.encrypt_guess(tid, guess, ckey)
+            result = wiiu_decrypt.decrypt(tid, encrypted_keyguess, ckey, contents, title_id, app_data)
+            if (result == 1):
+                print('bruteforce success after '+str(attempt)+' attempts')
+                print('password: '+guess)
+                print('encrypted titlekey: '+encrypted_keyguess)
+                print('decrypted titlekey: '+binascii.hexlify(unencrypted_keyguess).decode())
+                decoded_event.set()
+            time.sleep(0.2)
+        except:
+            if passes_done_event.is_set() and data_queue.empty():
+                print(f"[Worker {worker_id}] No more data and producer is done.")
+                break
+            continue
 
-if args.titleid:
-    # TODO: sanity checking
-    tid = args.titleid
-else:
-    sys.exit('Error: invalid titleid')
-
-system = ''
-if args.system:
-    system = args.system
-else:
-    sys.exit('Error: system autodetection based on titleid/content not yet implemented! please use the --system argument.')
-
-match system:
-    case 'wiiu':
-        print('system = wiiu')
-    case 'dsi':
-        print('system = dsi')
-    case '3ds':
-        sys.exit('Error: --system 3ds not yet implemented')
-    case 'wii':
-        sys.exit('Error: --system wii not yet implemented')
-    case _:
-        sys.exit('Error: invalid argument passed in --system')
-
-
-if args.commonkeyoverride:
-    ckey = args.commonkeyoverride
-else:
-    if args.commonkey:
-        with open('ckey.json', 'r') as f:
-            ckey = json.load(f)[args.commonkey+'_commonkey']
-        if ckey == '':
-            sys.exit('Error: failed to load '+args.commonkey+'_commonkey from ckey.json')
-        print('using '+args.commonkey+'_commonkey')
+def main():
+    manager = multiprocessing.Manager()
+    data_queue = manager.Queue(QUEUE_MAX_SIZE)
+    decoded_event = manager.Event()
+    passes_done_event = manager.Event()
+    
+    if args.titleid:
+        # TODO: sanity checking
+        tid = args.titleid
     else:
-        keyselect = ''
-        match system:
-            case 'wiiu':
-                keyselect = 'wiiu_dev_commonkey'
-            case 'dsi':
-                keyselect = 'dsi_dev_commonkey'
-            case '3ds':
-                keyselect = '3ds_dev_commonkey'
-            case 'wii':
-                keyselect = 'wii_dev_commonkey'
-            case _:
-                sys.exit('Error: unable to autoselect commonkey for unknown system \"'+args.system+'\"')
-        with open('ckey.json', 'r') as f:
-            ckey = json.load(f)[keyselect]
-        if ckey == '':
-            sys.exit('Error: failed to load '+keyselect+' from ckey.json')
-        print('using '+keyselect)
-
-match system:
-    case 'wiiu':
-        bruteforce_wiiu(tid, ckey)
-    case 'dsi':
-        bruteforce_dsi(tid, ckey)
-    case _:
-        print('system '+system+' is invalid or not yet implemented')
+        sys.exit('Error: invalid titleid')
+    
+    system = ''
+    if args.system:
+        system = args.system
+    else:
+        sys.exit('Error: system autodetection based on titleid/content not yet implemented! please use the --system argument.')
+    
+    match system:
+        case 'wiiu':
+            print('system = wiiu')
+        case 'dsi':
+            print('system = dsi')
+        case '3ds':
+            sys.exit('Error: --system 3ds not yet implemented')
+        case 'wii':
+            sys.exit('Error: --system wii not yet implemented')
+        case _:
+            sys.exit('Error: invalid argument passed in --system')
+    
+    
+    if args.commonkeyoverride:
+        ckey = args.commonkeyoverride
+    else:
+        if args.commonkey:
+            with open('ckey.json', 'r') as f:
+                ckey = json.load(f)[args.commonkey+'_commonkey']
+            if ckey == '':
+                sys.exit('Error: failed to load '+args.commonkey+'_commonkey from ckey.json')
+            print('using '+args.commonkey+'_commonkey')
+        else:
+            keyselect = ''
+            match system:
+                case 'wiiu':
+                    keyselect = 'wiiu_dev_commonkey'
+                case 'dsi':
+                    keyselect = 'dsi_dev_commonkey'
+                case '3ds':
+                    keyselect = '3ds_dev_commonkey'
+                case 'wii':
+                    keyselect = 'wii_dev_commonkey'
+                case _:
+                    sys.exit('Error: unable to autoselect commonkey for unknown system \"'+args.system+'\"')
+            with open('ckey.json', 'r') as f:
+                ckey = json.load(f)[keyselect]
+            if ckey == '':
+                sys.exit('Error: failed to load '+keyselect+' from ckey.json')
+            print('using '+keyselect)
+    
+    match system:
+        case 'wiiu':
+            bruteforce_wiiu(tid, ckey)
+        case 'dsi':
+            bruteforce_dsi(tid, ckey)
+        case _:
+            print('system '+system+' is invalid or not yet implemented')
